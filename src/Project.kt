d9adb77c4d78470299b53374cbd118e270693555
Dan <danielmoseley0@googlemail.com>
2020-11-05 13:18:12 +0000

    Testing Commit

--- src/Project.kt
+++ src/Project.kt
@@ -1,7 +1,8 @@
 import java.io.FileWriter
+import java.io.Serializable
 import java.util.Date
 
-data class Project(var projectName: String = "Project", var projectStartDate: Date, var projectEndDate: Date? = null, var projectDescription: String) {
+data class Project(var projectName: String = "Project", var projectStartDate: Date, var projectEndDate: Date? = null, var projectDescription: String) : Serializable{
     val Teams = mutableListOf<Team>();
     val Tasks = mutableListOf<Task>();
     init {
