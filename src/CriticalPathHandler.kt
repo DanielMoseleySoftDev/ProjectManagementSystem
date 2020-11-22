92e3908fc507b1784d51bc7aa7b5e3f8a25db591
Endryu <ipodendriu@gmail.com>
2020-11-22 15:29:21 +0000

    CriticalPathHandler.kt comments

--- src/CriticalPathHandler.kt
+++ src/CriticalPathHandler.kt
@@ -2,7 +2,6 @@ class CriticalPathHandler {
 
     var jobsList = ArrayList<Job>()
     var taskList = ArrayList<Task>()
-    //var criticalTasks = ArrayList<String>()
     var criticalTasks = arrayListOf<ArrayList<String>>()
     var criticalInfo = ArrayList<String>()
 
@@ -26,7 +25,7 @@ class CriticalPathHandler {
             println("CriticalPathHandler.CalcCriticalPath -> Kotlin Algorithm")
             returnJobs =  CriticalPathKotlin.calculateCriticalPath(jobSet)
             //Main.scalaHandler.calculateCriticalPath(jobSet)
-        }else{    //Todo Get rid of this else if. Can just be else
+        }else{
             //Scala
             println("CriticalPathHandler.CalcCriticalPath -> Scala Algorithm")
             returnJobs = Main.scalaHandler.calculateCriticalPath(jobSet)
@@ -40,8 +39,6 @@ class CriticalPathHandler {
 
     private fun toStringArray(returnJobs : Array<Job>) {
         println("Critical Tasks -> why we printing everything?")
-
-
         for (job in returnJobs){
             if(job.jobStatus != Status.COMPLETE){
                 var jobInfo = ArrayList<String>()
@@ -78,30 +75,24 @@ class CriticalPathHandler {
         //println(jobsList)
         var count = 0
         for (i in taskList){
-            println("Looking at ${i.taskName}")
+            //println("Looking at ${i.taskName}")
 
             var countB = 0
             for (j in taskList){
-                //var countB = 0
-                println("Looking at ${i.taskName} -> ${j.taskName}")
+                //println("Looking at ${i.taskName} -> ${j.taskName}")
                 for (preReq in j.preReqTasks){
-
                     if (preReq.taskName == i.taskName){
                         //println(jobsList[count].listOfChildren.size)
-                        println("found a child ${jobsList[countB].jobName}")
-
+                        //println("found a child ${jobsList[countB].jobName}")
                         jobsList[count].listOfChildren.add(jobsList[countB])
-
                     }
-                    //countB++
                 }
                 countB++
-
             }
             count++
             //println(jobsList[count].listOfParents)
         }
-        println(jobsList[0].listOfChildren)
+        //println(jobsList[0].listOfChildren)
         addStartAndEnd()
     }
 
@@ -121,8 +112,6 @@ class CriticalPathHandler {
                 job.listOfChildren.add(jobsList.last())
             }
         }
-        //jobsList.add(jobsList.size, Job( "END",0, finishingJobs))
-
     }
 
     private fun createJobsList() {
